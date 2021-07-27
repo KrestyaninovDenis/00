b2af4227aaef6b61153824f5ad22ace7f4243186
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-27 23:19:44 +0500

    Add files via upload

--- connectingBD/MODELS/1-2-Advertisement.js
+++ connectingBD/MODELS/1-2-Advertisement.js
@@ -34,8 +34,8 @@ const advertisementSchema = new Schema({
         default: "",   
     },    
     userId: {
-        type: mongoose.Schema.Types.ObjectId, 
-           
+        type: String,
+        default: "",
     },
     createdAt: {
         type: Date, 
