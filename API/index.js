a1f25e0d69f6982b967ae26311f175b179bed7e0
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-29 20:51:59 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -3,15 +3,7 @@ const router = express.Router();
 
 
 
-router.get('/api/signup', (req, res) => {
-/*    
-    const {books} = stor;
-    const {title, description, authors, favorite, fileCover, fileName, fileBook} = req.body;
-    const newBook = new Book(title, description, authors, favorite, fileCover, fileName, fileBook);
-    books.push(newBook);
-    res.status(201);
-    res.json(newBook);
-*/
+router.get('/api/signup', async (req, res) => {
 
 const createUser = require ('../connectingBD/BD-function').createUser;
 const data = ({ email:'11', passwordHash:'21', name:'31', contactPhone:'41'});
