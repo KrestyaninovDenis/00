73d8206fed4bc68b92eb03662156b0e0f0dee3d0
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-29 23:53:20 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -5,16 +5,13 @@ const bd = require ('../connectingBD/BD-function');
 router.post('/api/signup', async (req, res) => { 
     try{
 const createUser_END = await bd.createUser(req.body)
-if (!Object.keys(createUser_END).length) {
-res.json ({data:createUser_END,status:'OK'});
-}
-else 
-{
-res.json ({error:'email занят',status:'error'}); 
-}
+
+for (let key in createUser_END) {
+    res.json ({error:'email занят',status:'error'});
     }
-    catch {
+    res.json ({data:createUser_END,status:'OK'});
     }
+    catch {}
 });
 
 
