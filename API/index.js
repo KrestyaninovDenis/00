240e9670e28a27a03d933c75de1c5b61ad4405d7
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-29 21:54:12 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -12,7 +12,7 @@ router.get('/api/signup', async (req, res) => {
 router.post('/api/signup', async (req, res) => {
     const {email, passwordHash, name, contactPhone} = req.body;
 
-    const Book = require ('../connectingBD/MODELS/index').createUser;
+    const Book = require ('../connectingBD/MODELS/index').User;
     const data = new Book({
         email, passwordHash, name, contactPhone
     });
