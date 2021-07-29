8e477352052788805032f1f932d82dd2dc345d3e
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-30 00:02:40 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -6,14 +6,16 @@ router.post('/api/signup', async (req, res) => {
     try{
 const createUser_END = await bd.createUser(req.body)
 
-for (let key in createUser_END) {
+if (Object.keys(obj).length == 0) {
     res.json ({error:'email занят',status:'error'});
-    }
+}
+else{
     res.json ({data:createUser_END,status:'OK'});
+}
+
     }
     catch {}
 });
-
 
 
 
