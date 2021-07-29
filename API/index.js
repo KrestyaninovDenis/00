807f9fc1dfa63a562fa77fe6c2903dc91a0b6aad
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-29 21:22:56 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -2,14 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 
-
-router.get('/api/signup', async (req, res) => {});
-
 router.post('/api/signup', async (req, res) => {
 
     const createUser = require ('../connectingBD/BD-function').createUser;
     const data = req.body;
-    createUser(data).then(console.log);
+    createUser(data).then();
     });
 
 module.exports = router;
