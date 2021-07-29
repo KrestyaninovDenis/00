c76ab788880e91ffa43d7b70b65c0435b2f28ce3
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-29 22:18:39 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 router.get('/api/signup', async (req, res) => {
     const aaa = req;
+    res.json ('books');
     res.status(555)
 });
 
 router.post('/api/signup', async (req, res) => {
     console.log(req.body)
-
+    res.json (resoult);
 res.status(555);
 });
 
