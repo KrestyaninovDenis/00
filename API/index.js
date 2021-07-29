fd2d29399239221ff091e88beeedcc4e5e03acb6
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-29 23:42:10 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -5,11 +5,15 @@ const bd = require ('../connectingBD/BD-function');
 router.post('/api/signup', async (req, res) => { 
     try{
 const createUser_END = await bd.createUser(req.body)
+if (!Object.keys(createUser_END).length) {
 res.json ({data:createUser_END,status:'OK'});
-
+}
+else 
+{
+res.json ({error:'email занят',status:'error'}); 
+}
     }
     catch {
-        console.log("User profile not found") 
     }
 });
 
