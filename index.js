3bfb21e8c5faff1d9060fca086c3fdd4e240b5a8
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-30 22:59:16 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ const passport       = require('passport');
 const LocalStrategy  = require('passport-local').Strategy;
 
 passport.use('local', new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password',
+    usernameField: 'email',
+    passwordField: 'passwordHash',
     passReqToCallback : false
   },
       function(username, password, done){
