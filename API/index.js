d06eaeec4c79ad4d5855361afb01fbefef8cb1c5
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-07-31 14:06:50 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -44,12 +44,10 @@ router.post('/api/advertisements', async (req, res) => {
 // Поиск объявления (для всех)
 router.get('/api/advertisements/:id', async (req, res) => {
     try {
-        const RRR = req.params.id//"id": "610510330b59160012f2daa5"
-        const rr1 = ({_id:RRR})
-        //const ADV = await bd.findAdvertisement(RRR)
+        const RRR = ({_id:req.params.id})
+        const ADV = await bd.findAdvertisement(RRR)
         res.status (200);
-    res.json (rr1)
-        //res.json ({data:ADV,status:'OK'});
+        res.json ({data:ADV,status:'OK'});
     }
     catch {
         res.status (404);
