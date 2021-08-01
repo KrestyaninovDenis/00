70401506c7166db034af8638ba8e83a5b62f6cc0
KrestyaninovDenis <85801184+KrestyaninovDenis@users.noreply.github.com>
2021-08-01 13:08:28 +0500

    Add files via upload

--- API/index.js
+++ API/index.js
@@ -1,4 +1,4 @@
-//const express   = require('express');
+const express   = require('express');
 //const app       = express();
 const router    = express.Router();
 const bd        = require ('../connectingBD/BD-function');
